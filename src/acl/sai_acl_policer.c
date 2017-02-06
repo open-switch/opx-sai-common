dc83f2827536735643174d5923078ca9c81da15f
J.T. Conklin <J.T.Conklin@Dell.Com>
2017-02-05 17:04:41 -0800

    Remove conflicting copyright boilerplate
    Signed-off-by: J.T. Conklin <J.T.Conklin@Dell.Com>

--- src/acl/sai_acl_policer.c
+++ src/acl/sai_acl_policer.c
@@ -1,18 +1,25 @@
-/************************************************************************
- *  LEGALESE:   "Copyright (c) 2015, Dell Inc. All rights reserved."
+/*
+ * Copyright (c) 2016 Dell Inc.
  *
- *  This source code is confidential, proprietary, and contains trade
- *  secrets that are the sole property of Dell Inc.
- *  Copy and/or distribution of this source code or disassembly or reverse
- *  engineering of the resultant object code are strictly forbidden without
- *  the written consent of Dell Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- ************************************************************************
- *  @file sai_acl_policer.c
+ * THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ * LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS
+ * FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
  *
- *  @brief This file contains functions for SAI ACL policer operations.
+ * See the Apache Version 2.0 License for specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * @file sai_acl_policer.c
  *
- *************************************************************************/
+ * @brief This file contains functions for SAI ACL policer operations.
+ */
+
 #include "sai_acl_utils.h"
 #include "sai_qos_util.h"
 #include "sai_acl_type_defs.h"
